2b435d0d96d37c7cd61b8b9e0477dc3a37fc8567
Laucha19 <115588529+Laucha19@users.noreply.github.com>
2024-07-21 16:38:30 -0400

    Add files via upload

--- PythonCrashCourse/Alien Invasion Project/alien_invasion.py
+++ PythonCrashCourse/Alien Invasion Project/alien_invasion.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
-from character import Fox
+
 
 def run_game():
     # Initialize pygame, settings, and screen object.
@@ -14,12 +16,18 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
-    fox = Fox(screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+    aliens = Group()
+
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship, fox)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 run_game()
